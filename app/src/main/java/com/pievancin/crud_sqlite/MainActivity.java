aecef5f539bbac4bfcd58347233160974524e255
Pierre Vancin <pie.vancin@gmail.com>
2021-10-28 20:06:11 -0300

    Initial commit

---
+++ app/src/main/java/com/pievancin/crud_sqlite/MainActivity.java
@@ -0,0 +1,31 @@
+package com.pievancin.crud_sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText nome;
+    private EditText cpf;
+    private EditText telefone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nome = findViewById(R.id.editNome);
+        cpf = findViewById(R.id.editCPF);
+        telefone = findViewById(R.id.editTelefone);
+    }
+
+    public void salvar(View view) {
+        Aluno a = new Aluno();
+        a.setNome(nome.getText().toString());
+        a.setCpf(cpf.getText().toString());
+        a.setTelefone(telefone.getText().toString());
+    }
+}
