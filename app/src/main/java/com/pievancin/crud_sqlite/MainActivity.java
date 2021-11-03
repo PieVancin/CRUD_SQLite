a6cdc168c32c2b3e13c83076f36f88df22014d03
Pierre Vancin <pie.vancin@gmail.com>
2021-11-03 20:18:43 -0300

    Read dos nomes implementado

--- app/src/main/java/com/pievancin/crud_sqlite/MainActivity.java
+++ app/src/main/java/com/pievancin/crud_sqlite/MainActivity.java
@@ -2,8 +2,10 @@ package com.pievancin.crud_sqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText telefone;
     private AlunoDAO dao;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
         cpf = findViewById(R.id.editCPF);
         telefone = findViewById(R.id.editTelefone);
         dao = new AlunoDAO(this);
+        Button listar = (Button) findViewById(R.id.button2);
+
+        listar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                Intent it = new Intent(MainActivity.this, ListarAlunosActivity.class);
+                startActivity(it);
+            }
+        });
     }
 
     public void salvar(View view) {
